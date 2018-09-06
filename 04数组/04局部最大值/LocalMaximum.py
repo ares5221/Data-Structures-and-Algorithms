225138f9c4015e62c673491c034cd2158a1095f3
liujiefei <674361437@qq.com>
2018-09-06 09:09:12 +0800

    firstmissnum

--- 04数组/04局部最大值/LocalMaximum.py
+++ 04数组/04局部最大值/LocalMaximum.py
@@ -5,10 +5,16 @@
 """
 def LocalMaximum(src):
     srclen = len(src)
-
-    return srclen
+    left, right = 0, len(src) -1
+    while(left < right):
+        mid = int((left + right) / 2)
+        if src[mid] > src[mid +1]:
+            right = mid
+        else:
+            left = mid +1
+    return src[left]
 
 if __name__ == '__main__':
-    src = [2, 3, 6, 8, 12, 5, 6, 7, 9, 15, 32, 43]
+    src = [2, 3, 6, 8, 12, 5, 6, 7, 9, 15, 32, 18]
     localMax = LocalMaximum(src)
-    print(localMax)
+    print('找到的局部最大值之一是：', localMax)
